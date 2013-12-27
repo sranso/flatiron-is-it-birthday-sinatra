39edd31b381386c14f7f1049dba0197dc6ec0642
sranso <sranso@gmail.com>
2013-12-27 13:49:15 -0500

    fix argument error

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ class Holiday
   end
 
   def countdown(month, day)
-    time_diff = (@time - Time.local(next_holiday_year(month), month, day)).to_i.abs
+    time_diff = (@time - Time.local(next_holiday_year(month, day), month, day)).to_i.abs
     # 86400 = seconds in day
     days_until = time_diff/86400
     remainder = time_diff%86400
